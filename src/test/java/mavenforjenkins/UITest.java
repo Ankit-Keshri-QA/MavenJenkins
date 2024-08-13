8a7065dd98258814b64621deda97d9e85d4e5819
Ankit Keshri <ankit_keshri@epam.com>
2024-08-13 16:04:20 +0530

    New Commit - Updated UITest Java Code

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -4,23 +4,42 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UITest {
 
+	@Parameters("browser")
 	@Test
-	public void startBrowser() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+	public void startBrowser(String browserName) {
+
+		System.out.println("Parameter value is + : " + browserName);
+		WebDriver driver;
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+
+		else if (browserName.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+
+		} else {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://blazedemo.com/");
 
 		Assert.assertEquals(driver.getTitle(), "BlazeDemo", "Title Does not match");
-
 		driver.close();
 
 	}
